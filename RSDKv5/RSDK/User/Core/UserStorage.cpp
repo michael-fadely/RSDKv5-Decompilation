8ab0bdfacdd1b96269efd6ebf579b74ef007d16b
Michael Fadely <michaelwfadely@gmail.com>
2022-09-02 21:32:48 -0700

    set file prefix to /cd/

--- RSDKv5/RSDK/User/Core/UserStorage.cpp
+++ RSDKv5/RSDK/User/Core/UserStorage.cpp
@@ -1163,6 +1163,10 @@ void RSDK::SKU::InitUserDirectory()
 
     SKU::SetUserFileCallbacks("./", NULL, NULL);
 
+#elif RETRO_PLATFORM == RETRO_KALLISTIOS
+
+    SKU::SetUserFileCallbacks("/cd/", NULL, NULL);
+
 #else
 
     SKU::SetUserFileCallbacks("", NULL, NULL);
