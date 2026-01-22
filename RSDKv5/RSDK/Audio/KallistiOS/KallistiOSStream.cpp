10021e24e62d7955a46d727df8749c822d7078c7
jnmartin84 <jnmartin84@gmail.com>
2026-01-22 15:42:27 -0500

    recover 32kb of RAM from streaming thread

--- RSDKv5/RSDK/Audio/KallistiOS/KallistiOSStream.cpp
+++ RSDKv5/RSDK/Audio/KallistiOS/KallistiOSStream.cpp
@@ -131,7 +131,7 @@ int stream_init(void)
     stream.callback = NULL;
     audio_attr.create_detached = 0;
     // if you make this smaller, it might overrun and crash everything
-    audio_attr.stack_size = 65536;
+    audio_attr.stack_size = 32768;
     audio_attr.stack_ptr = NULL;
     audio_attr.prio = PRIO_DEFAULT;
     audio_attr.label = "MusicPlayer";
