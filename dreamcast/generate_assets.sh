3185fdbcf5a85aade974897a9548070a58ec63ee
jnmartin84 <jnmartin84@gmail.com>
2026-01-22 23:44:59 -0500

    bug-fix scripts

--- dreamcast/generate_assets.sh
+++ dreamcast/generate_assets.sh
@@ -8,12 +8,17 @@ cp -r "$1"/Sprites/Global "$2"/Sprites
 cp -r "$1"/Sprites/TMZ1 "$2"/Sprites
 cp -r "$1"/Sprites/UI "$2"/Sprites
 
-./gfx_step_1_toalphapng.sh "$2"
-./gfx_step_2_todtex.sh "$2"
-./music_step_1_ogg_to_pcm.sh "$2"
-./music_step_2_rename_to_ogg.sh "$2"
 ./sfx_step_1_downsample.sh "$2"
 ./sfx_step_2_22kwav2adpcm.sh "$2"
 ./sfx_step_3_22k.sh "$2"
 ./sfx_step_4.sh "$2"
+
+./music_step_1_ogg_to_pcm.sh "$2"
+./music_step_2_rename_to_ogg.sh "$2"
+
+./gfx_step_1_toalphapng.sh "$2"
+./gfx_step_2_todtex.sh "$2"
+
+exit 0
+
 
