44aea38b2b38ac524ae7489030496a838d421243
jnmartin84 <jnmartin84@gmail.com>
2026-01-26 16:52:37 -0500

    use realpath

--- dreamcast/generate_assets.sh
+++ dreamcast/generate_assets.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+sourcedir=$(realpath "$1")
+
 mkdir -p "$2"
-cp -r "$1"/Music "$2"
-cp -r "$1"/SoundFX "$2"
-mkdir -p "$2"/Sprites
-cp -r "$1"/Sprites/Global "$2"/Sprites
-cp -r "$1"/Sprites/TMZ1 "$2"/Sprites
-cp -r "$1"/Sprites/UI "$2"/Sprites
 
-./sfx_step_1_downsample.sh "$2"
-./sfx_step_2_wav2adpcm.sh "$2"
-./sfx_step_3_cleanup.sh "$2"
-./sfx_step_4_move.sh "$2"
+stagedir=$(realpath "$2")
 
-./music_step_1_ogg_to_pcm.sh "$2"
-./music_step_2_rename_to_ogg.sh "$2"
+cp -r "$sourcedir"/Music "$stagedir"
+cp -r "$sourcedir"/SoundFX "$stagedir"
+mkdir -p "$stagedir"/Sprites
+cp -r "$sourcedir"/Sprites/Global "$stagedir"/Sprites
+cp -r "$sourcedir"/Sprites/TMZ1 "$stagedir"/Sprites
+cp -r "$sourcedir"/Sprites/UI "$stagedir"/Sprites
 
-./gfx_step_1_toalphapng.sh "$2"
-./gfx_step_2_todtex.sh "$2"
+./sfx_step_1_downsample.sh "$stagedir"
+./sfx_step_2_wav2adpcm.sh "$stagedir"
+./sfx_step_3_cleanup.sh "$stagedir"
+./sfx_step_4_move.sh "$stagedir"
+
+./music_step_1_ogg_to_pcm.sh "$stagedir"
+./music_step_2_rename_to_ogg.sh "$stagedir"
+
+./gfx_step_1_toalphapng.sh "$stagedir"
+./gfx_step_2_todtex.sh "$stagedir"
 
 echo "-- DONE --"
 
