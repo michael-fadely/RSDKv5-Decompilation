337e6cd275a1145fb9e03385dc696ee778590521
jnmartin84 <jnmartin84@gmail.com>
2026-02-03 14:06:23 -0500

    fix STR pool OOM in credits sequence

--- RSDKv5/RSDK/Storage/Storage.cpp
+++ RSDKv5/RSDK/Storage/Storage.cpp
@@ -88,9 +88,9 @@ bool32 RSDK::InitStorage()
 #if RETRO_PLATFORM == RETRO_KALLISTIOS  /* SAYGA DREAMCAST: Where Sonic Belongs! */
                                                                        // RAM: 32  / 16  MB
                                                                        // -----------------
-    dataStorage[DATASET_STG].storageLimit = 400000 + (DBL_MEM? 4 : 4) * 1024 * 1024; // 4   / 4   MB
-    dataStorage[DATASET_STR].storageLimit = (DBL_MEM? 2 : 2) *  32 * 1024; // 64  / 32  KB
-    dataStorage[DATASET_TMP].storageLimit = 100000 + (DBL_MEM? 6 : 3) * 1024 * 1024; // 6   / 3   MB
+    dataStorage[DATASET_STG].storageLimit = 400000 + (DBL_MEM? 4 : 4) * 1024 * 1024; // 4.4   / 4.4   MB
+    dataStorage[DATASET_STR].storageLimit = (DBL_MEM? 3 : 3) *  32 * 1024; // 96  / 96  KB
+    dataStorage[DATASET_TMP].storageLimit = 100000 + (DBL_MEM? 6 : 3) * 1024 * 1024; // 6.1   / 3.1   MB
                                                                      //   -----------------
 #else                                                                // Total: 16+ / 7+  MB 
                                         /* PCs AND BORING SHIT */
