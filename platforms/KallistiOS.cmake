b36cfa5c3c59f519f97cb2222e0dcc30437ab847
Falco Girgis <gyrovorbis@gmail.com>
2026-03-02 15:22:03 -0600

    CMakeLists uses -Os globally, -O3 selectively. (#6)
    * CMakeLists uses -Os globally, -O3 selectively.
    
    For maximum gainz, we're leveragig a trick that I've used with every bit
    port I've worked on that both conserves RAM *and* improves performance.
    
    First, we compile everything globally as -Os by default, giving us as
    small of a binary as possible, conserving our RAM.
    
    Second, we create a list of "CRITICAL_FILES" in CMake that we use to
    explicitly list out high-performance hot path files which we then give
    the -O3 treatment, prioritizing speed above all else.
    
    The initial list contains a bunch of stuff for rendering, math, and
    collision detection.
    
    * move CRITICAL_FILES
    do the opt flag stuff in platforms/KallistiOS.cmake
    
    * restore CMakeLists.txt
    
    * fix line end
    
    ---------
    
    Co-authored-by: jnmartin84 <jnmartin84@gmail.com>

--- platforms/KallistiOS.cmake
+++ platforms/KallistiOS.cmake
@@ -24,3 +24,50 @@ target_compile_definitions(RetroEngine PUBLIC KOS_USER_DIR="${KOS_USER_DIR}")
 # Disable some unneeded features to reduce executable size
 set(RETRO_MOD_LOADER OFF CACHE BOOL "Disable mod loader" FORCE)
 set(GAME_INCLUDE_EDITOR OFF CACHE BOOL "Disable unused editor code" FORCE)
+
+if(NOT RSDK_DEBUG)
+    # Translation units with hot code which needs high optimization levels.
+    set(CRITICAL_FILES
+        RSDKv5/RSDK/Core/Math.cpp
+        RSDKv5/RSDK/Graphics/Drawing.cpp
+        RSDKv5/RSDK/Graphics/Scene3D.cpp
+        RSDKv5/RSDK/Scene/Collision.cpp
+        RSDKv5/RSDK/Scene/Scene.cpp
+    )
+
+    set(OTHER_FILES
+        RSDKv5/main.cpp
+        RSDKv5/RSDK/Core/RetroEngine.cpp
+        RSDKv5/RSDK/Core/Reader.cpp
+        RSDKv5/RSDK/Core/Link.cpp
+        RSDKv5/RSDK/Core/ModAPI.cpp
+        RSDKv5/RSDK/Dev/Debug.cpp
+        RSDKv5/RSDK/Storage/Storage.cpp
+        RSDKv5/RSDK/Storage/Text.cpp
+        RSDKv5/RSDK/Graphics/Animation.cpp
+        RSDKv5/RSDK/Graphics/Sprite.cpp
+        RSDKv5/RSDK/Graphics/Palette.cpp
+        RSDKv5/RSDK/Graphics/Video.cpp
+        RSDKv5/RSDK/Audio/Audio.cpp
+        RSDKv5/RSDK/Input/Input.cpp
+        RSDKv5/RSDK/Scene/Object.cpp
+        RSDKv5/RSDK/Scene/Objects/DefaultObject.cpp
+        RSDKv5/RSDK/Scene/Objects/DevOutput.cpp
+        RSDKv5/RSDK/User/Core/UserAchievements.cpp
+        RSDKv5/RSDK/User/Core/UserCore.cpp
+        RSDKv5/RSDK/User/Core/UserLeaderboards.cpp
+        RSDKv5/RSDK/User/Core/UserPresence.cpp
+        RSDKv5/RSDK/User/Core/UserStats.cpp
+        RSDKv5/RSDK/User/Core/UserStorage.cpp
+        dependencies/all/tinyxml2/tinyxml2.cpp
+        dependencies/all/iniparser/iniparser.cpp
+        dependencies/all/iniparser/dictionary.cpp
+        dependencies/all/miniz/miniz.c
+    )
+
+    # Force our list of critical files to get built with -O3 optimizaiton levels.
+    set_source_files_properties(${CRITICAL_FILES} PROPERTIES COMPILE_FLAGS "-O3")
+
+    # Force our list of other files to get built with -Os optimizaiton levels.
+    set_source_files_properties(${OTHER_FILES} PROPERTIES COMPILE_FLAGS "-Os")
+endif()
