f30be6948eb7364074da618e6a2440c9696819aa
Michael Fadely <michaelwfadely@gmail.com>
2026-04-09 11:01:24 -0600

    Replace modelbake.py and strippy with mania-mesh-optimizer (#55)
    * replace modelbake.py and strippy with mania-mesh-optimizer
    
    * use rm, not rmdir
    
    * update mania-mesh-optimizer submodule
    
    * mark model_process.sh as executable
    
    * update mania-mesh-optimizer submodule
    
    * adjust comments for RSDK::LoadMesh changes
    
    * LoadMesh: use explicit type for LoadFile return value

--- dreamcast/generate_assets.sh
+++ dreamcast/generate_assets.sh
@@ -22,15 +22,6 @@ rsdk_file=$1
 src_in=$2
 stage_in=$3
 
-cd strippy
-make clean
-make
-cp strippy ../tristrip
-cd ..
-
-python3 ./modelbake.py "$rsdk_file"
-./tristrip "$rsdk_file"
-
 mkdir -p -- "$src_in"
 
 python3 rsdkv5_extract.py "$rsdk_file" "$src_in"
@@ -74,6 +65,8 @@ cp -R -- "$sourcedir/Sprites/UI"            "$stagedir/Sprites/"
 
 # one non-pow2 file -_-
 rm -f -- "$stagedir/Sprites/UI/Achievements.gif"
+
+"$script_dir/model_process.sh"              "$sourcedir" "$stagedir"
 
 "$script_dir/video_script.sh"               "$sourcedir" "$stagedir"
 
